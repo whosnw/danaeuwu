842fe640c7754d256aaa6996f118ff98c772aa57
whosnw <almakts97@gmail.com>
2026-03-06 01:19:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('feliz Cumpleaños Danae ‹3').split('')
+    const titles = ('Feliz Cumpleaños Danae ‹3').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
